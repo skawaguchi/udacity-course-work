fc0cc667a7f6cf040ad5d1180d527224b85364a9
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2015-07-20 08:30:58 -0400

    Added actor class

---
+++ project_01_movies_trailers/code/actor.py
@@ -0,0 +1,4 @@
+class Actor():
+
+    def __init__(self, first_name, last_name):
+        self.name = first_name + " " + last_name
--- project_01_movies_trailers/code/media.py
+++ project_01_movies_trailers/code/media.py
@@ -5,12 +5,13 @@ class Movie():
     # Default to a 'no image' image instead of an ugly missing image or something
     NO_IMAGE_PATH="/img/no_image.png"
 
-    def __init__(self, film_title, film_synopsis, film_poster_url, film_trailer_url):
-        
+    def __init__(self, film_title, film_synopsis, film_poster_url, film_trailer_url, film_actors):
+
         self.title = film_title
         self.synopsis = film_synopsis
         self.poster_image_url = film_poster_url
         self.trailer_youtube_url = film_trailer_url
+        self.actors = film_actors
 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
