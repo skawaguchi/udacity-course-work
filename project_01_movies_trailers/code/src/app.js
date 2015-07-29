c71f3968dee0a9fcdfc2a4a0f28435d42da606af
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2015-07-29 10:23:54 -0400

    More eslint cleanup. Also introduced more rules.

--- project_01_movies_trailers/code/src/app.js
+++ project_01_movies_trailers/code/src/app.js
@@ -1,47 +1,21 @@
 
 /* global $ movies */
 
-window.freshTomatoes = (function (movies) {
+window.freshTomatoes = (function () {
   'use strict';
 
   var
-    trailerVideoContainer = $('#trailer-video-container'),
-    movieDetailContainer = $('#movie-detail-container');
+    trailerVideoContainer = $('#trailer-video-container');
 
-  function modalClicked () {
-    // Remove the src so the player itself gets removed, as this is the only
-    // reliable way to ensure the video stops playing in IE
-    trailerVideoContainer.empty();
+  function getMovieInfo(movieId) {
+    return movies.find(function (movie) {
+      if (movie.id === movieId) {
+        return movie;
+      }
+    });
   }
 
-  function movieTileMouseOver () {
-    var
-      targetElement = $(this),
-      movieId = targetElement.attr('data-movie-id');
-
-    showMovieDetails(targetElement, movieId);
-  }
-
-  function movieTileClicked () {
-    var trailerYouTubeId = $(this).attr('data-trailer-youtube-id');
-    playTrailer(trailerYouTubeId);
-  }
-
-  function playTrailer (trailerYouTubeId) {
-    var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-
-    trailerVideoContainer
-      .empty()
-      .append($('<iframe></iframe>', {
-        'id': 'trailer-video',
-        'type': 'text-html',
-        'src': sourceUrl,
-        'frameborder': 0
-      })
-    );
-  }
-
-  function showMovieDetails (targetElement, movieId) {
+  function showMovieDetails(targetElement, movieId) {
     var
       movie = getMovieInfo(movieId),
       popoverOptions = {
@@ -60,29 +34,59 @@ window.freshTomatoes = (function (movies) {
 
   }
 
-  function getMovieInfo (movieId) {
-    return movies.find(function (movie) {
-      if (movie.id === movieId) {
-        return movie;
-      }
-    });
+  function playTrailer(trailerYouTubeId) {
+    var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+
+    trailerVideoContainer
+      .empty()
+      .append($('<iframe></iframe>', {
+        'id': 'trailer-video',
+        'type': 'text-html',
+        'src': sourceUrl,
+        'frameborder': 0
+      })
+    );
   }
 
+  function modalClicked() {
+    // Remove the src so the player itself gets removed, as this is the only
+    // reliable way to ensure the video stops playing in IE
+    trailerVideoContainer.empty();
+  }
 
-  function init () {
+  function movieTileMouseOver() {
+    var
+      targetElement = $(this),
+      movieId = targetElement.attr('data-movie-id');
 
+    showMovieDetails(targetElement, movieId);
+  }
+
+  function movieTileClicked() {
+    var trailerYouTubeId = $(this).attr('data-trailer-youtube-id');
+    playTrailer(trailerYouTubeId);
+  }
+
+  function setUpModalEvents() {
     // Pause the video when the modal is closed
     $(document).on('click', '.hanging-close, .modal-backdrop, .modal', modalClicked);
+  }
 
+  function setUpMovieTileHandlers() {
     $(document).on('mouseover', '.movie-tile a', movieTileMouseOver);
 
     // Start playing the video whenever the trailer modal is opened
     $(document).on('click', '.movie-tile a', movieTileClicked);
+  }
 
+
+  function init() {
+    setUpModalEvents();
+    setUpMovieTileHandlers();
   }
 
   return {
     init: init
   };
 
-}(movies));
+}());
