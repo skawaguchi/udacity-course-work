51806962eb6b71eceb166d102984ba737bd8e89f
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2015-07-31 12:35:43 -0400

    chore: Cleaned up js code and added comments

--- project_01_movies_trailers/code/dist/app.js
+++ project_01_movies_trailers/code/dist/app.js
@@ -58,6 +58,11 @@
 /* 1 */
 /***/ function(module, exports) {
 
+	// Note to reviewer: I prefer a minimally commented style of coding as prefer
+	// Bob Martin's recommendation. I'll add comments here since that's the
+	// exercise, but will rely on the most obvious names to communicate intention.
+
+	// Inject global jQuery via IIFE
 	'use strict';
 
 	Object.defineProperty(exports, '__esModule', {
@@ -65,21 +70,18 @@
 	});
 	var FreshTomatoes = (function ($) {
 
-	  var privateData = new WeakMap(),
-	      privateObject = {};
-
+	  // The string template that we'll be using to call YouTube
 	  var YOU_TUBE_URL = 'http://www.youtube.com/embed/{{trailerYouTubeId}}?autoplay=1&html5=1';
 
-	  function FreshTomatoes(movies) {
-	    privateData.set(privateObject, {
-	      movies: movies
-	    });
+	  // Initialization
+	  function FreshTomatoes() {
 	    setUpModalHandlers();
 	    setUpMovieTilePopover();
 	    setUpMovieTileClickHandler();
 	  }
 
 	  function playTrailer(trailerYouTubeId) {
+	    // Use regex to insert the youtube id into the url string
 	    var sourceUrl = YOU_TUBE_URL.replace(/\{\{trailerYouTubeId\}\}/, trailerYouTubeId);
 
 	    $('#trailer-video-container').empty().append($('<iframe></iframe>', {
@@ -99,7 +101,9 @@
 	  }
 
 	  function movieTileClicked() {
-	    var targetEl = $(this),
+	    var
+	    // Store the DOM reference to minimize DOm calls
+	    targetEl = $(this),
 	        trailerYouTubeId = targetEl.attr('data-trailer-youtube-id');
 
 	    playTrailer(trailerYouTubeId);
@@ -110,6 +114,7 @@
 	  }
 
 	  function setUpMovieTilePopover() {
+	    // Bind the popover. Uses the markup for configuration and data.
 	    $('.movie-tile a').popover();
 	  }
 
--- project_01_movies_trailers/code/src/FreshTomatoes.js
+++ project_01_movies_trailers/code/src/FreshTomatoes.js
@@ -1,23 +1,27 @@
+// Note to reviewer: I prefer a minimally commented style of coding as prefer
+// Bob Martin's recommendation. I'll add comments here since that's the
+// exercise, but will rely on the most obvious names to communicate intention.
+
+// Inject global jQuery via IIFE
 var FreshTomatoes = (function ($) {
 
-  var
-    privateData = new WeakMap(),
-    privateObject = {};
-
+  // The string template that we'll be using to call YouTube
   const
     YOU_TUBE_URL = 'http://www.youtube.com/embed/{{trailerYouTubeId}}?autoplay=1&html5=1';
 
-  function FreshTomatoes(movies) {
-    privateData.set(privateObject, {
-      movies: movies
-    });
+  // Initialization
+  function FreshTomatoes() {
     setUpModalHandlers();
     setUpMovieTilePopover();
     setUpMovieTileClickHandler();
   }
 
   function playTrailer(trailerYouTubeId) {
-    var sourceUrl = YOU_TUBE_URL.replace(/\{\{trailerYouTubeId\}\}/, trailerYouTubeId);
+    // Use regex to insert the youtube id into the url string
+    var sourceUrl = YOU_TUBE_URL.replace(
+      /\{\{trailerYouTubeId\}\}/,
+      trailerYouTubeId
+    );
 
     $('#trailer-video-container')
       .empty()
@@ -40,6 +44,7 @@ var FreshTomatoes = (function ($) {
 
   function movieTileClicked() {
     var
+      // Store the DOM reference to minimize DOm calls
       targetEl = $(this),
       trailerYouTubeId = targetEl.attr('data-trailer-youtube-id');
 
@@ -47,10 +52,15 @@ var FreshTomatoes = (function ($) {
   }
 
   function setUpModalHandlers() {
-    $(document).on('click', '.hanging-close, .modal-backdrop, .modal', modalClicked);
+    $(document).on(
+      'click',
+      '.hanging-close, .modal-backdrop, .modal',
+      modalClicked
+    );
   }
 
   function setUpMovieTilePopover() {
+    // Bind the popover. Uses the markup for configuration and data.
     $('.movie-tile a').popover();
   }
 
